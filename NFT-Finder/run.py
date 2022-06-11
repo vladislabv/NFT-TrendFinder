acb82ebbd82b03c7ffd7ce6fcbb9863a029e07b6
Vladislav Stasenko <77554807@fsv.cuni.cz>
2022-06-11 14:33:36 +0200

    remove previous version of project

--- NFT-Finder/run.py
+++
@@ -1,7 +0,0 @@
-# Run a test server.
-#!flask/bin/python
-from __future__ import absolute_import, unicode_literals
-from nft_finder import app
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', debug=True)
